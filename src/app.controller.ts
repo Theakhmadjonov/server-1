a993fd630df6c4f9dd2d167525071154a2875e66
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-06-03 23:34:10 +0500

    last dancecc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
   getHello(): string {
+    console.log(1);
     return this.appService.getHello();
   }
 }
