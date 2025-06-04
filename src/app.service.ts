395823fcd42500c1a3a15231052b8130bf5f5430
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-06-04 14:07:45 +0500

    last dancesssss

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log(1);
-    return 'Hello to Server-1';
+    return 'Hello to Server-111';
   }
 }
